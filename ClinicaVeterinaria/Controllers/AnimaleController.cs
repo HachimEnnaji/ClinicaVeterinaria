404807b5896e86e368d489ddb2aefc833a4fbcf8
FrancescoVenanti <francesco.venanti@gmail.com>
2024-03-18 13:03:09 +0100

    secondo

---
+++ ClinicaVeterinaria/Controllers/AnimaleController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClinicaVeterinaria.Models;
+using Microsoft.Ajax.Utilities;
+
+namespace ClinicaVeterinaria.Controllers
+{
+    public class AnimaleController : Controller
+    {
+        private ModelDbContext db = new ModelDbContext();
+
+        // GET: Animale
+        public ActionResult Index()
+        {
+            return View(db.Animale.ToList());
+        }
+
+        // GET: Animale/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animale animale = db.Animale.Find(id);
+            if (animale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animale);
+        }
+
+        // GET: Animale/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animale/Create
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdAnimale,DataRegistrazione,Nome,Tipo,Colore,DataNascita,Microchip,Propietario,Foto")] Animale animale)
+        {
+            if (ModelState.IsValid)
+            {
+                if (animale.Foto.IsNullOrWhiteSpace())
+                {
+                    animale.Foto = "AnimaleDefault.jpg";
+                }
+                if (animale.DataNascita < DateTime.Now)
+                {
+                    TempData["success"] = $"{animale.Nome} aggiunto con successo";
+                    db.Animale.Add(animale);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["error"] = $"{animale.DataNascita} La data di nascita non può essere posteriore a {DateTime.Now}";
+                    return View();
+                }
+            }
+            TempData["error"] = "Errore nell'inserimento";
+            return View();
+
+        }
+
+        // GET: Animale/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animale animale = db.Animale.Find(id);
+            if (animale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animale);
+        }
+
+        // POST: Animale/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdAnimale,DataRegistrazione,Nome,Tipo,Colore,DataNascita,Microchip,Propietario,Foto")] Animale animale)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(animale).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(animale);
+        }
+
+        // GET: Animale/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Animale animale = db.Animale.Find(id);
+            if (animale == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animale);
+        }
+
+        // POST: Animale/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Animale animale = db.Animale.Find(id);
+            db.Animale.Remove(animale);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ClinicaVeterinaria/Models/Animale.cs
+++ ClinicaVeterinaria/Models/Animale.cs
@@ -20,7 +20,10 @@ namespace ClinicaVeterinaria.Models
         public int IdAnimale { get; set; }
 
         [Column(TypeName = "date")]
-        public DateTime DataRegistrazione { get; set; }
+        [Display(Name = "Data Registrazione")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DataRegistrazione { get; set; } = DateTime.Now;
 
         [Required]
         [StringLength(50)]
@@ -35,6 +38,8 @@ namespace ClinicaVeterinaria.Models
         public string Colore { get; set; }
 
         [Column(TypeName = "date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime DataNascita { get; set; }
 
         [StringLength(15)]
@@ -42,10 +47,11 @@ namespace ClinicaVeterinaria.Models
 
         [Required]
         [StringLength(200)]
-        public string Propietario { get; set; }
+        [Display(Name = "Proprietario")]
+        public string Propietario { get; set; } = "Rifugio";
 
         [StringLength(50)]
-        public string Foto { get; set; }
+        public string Foto { get; set; } = "AnimaleDefault.jpg";
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Ricovero> Ricovero { get; set; }
